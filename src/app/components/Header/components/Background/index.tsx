69d8ad40cd10b1bc7b89ca571ad95ddc1dafe0b2
Lorenzo <lorenzo.acetii@gmail.com>
2023-07-22 14:17:56 -0300

    Create floating menu

--- src/app/components/Header/components/Background/index.tsx
+++ src/app/components/Header/components/Background/index.tsx
@@ -4,12 +4,12 @@ type BackgroundProps = {
 
 export default function Background({ children }: BackgroundProps) {
   return (
-    <>
+    <div className="relative">
       <header className="bg-gradient-to-r from-rose-800 to-rose-500 w-full h-[592px] px-40 pt-5">
         {children}
       </header>
 
       <div className="w-0 h-0 -mt-[8rem] border-l-[100vw] border-l-transparent border-b-[8rem] border-solid border-b-neutral-200"></div>
-    </>
+    </div>
   )
 }
