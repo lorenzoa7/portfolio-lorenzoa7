b449f12342c96b9e9314956a70be1db1b7cee2de
Lorenzo <lorenzo.acetii@gmail.com>
2023-07-26 18:55:11 -0300

    Create contact section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="px-40 mt-20">
         <Skills />
         <Projects />
+        <Contact />
       </main>
       <footer></footer>
     </>
