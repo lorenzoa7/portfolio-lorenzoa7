f58146486b7c74e3af92c06306fd41f1d3701bcb
Lorenzo <lorenzo.acetii@gmail.com>
2023-07-31 14:15:58 -0300

    Fix image size

--- src/app/components/Projects/components/Project/ProjectImage.tsx
+++ src/app/components/Projects/components/Project/ProjectImage.tsx
@@ -3,18 +3,11 @@ import Image from 'next/image'
 type ProjectImageProps = {
   src: string
   alt: string
-  width?: number
-  height?: number
 }
 
-export default function ProjectImage({
-  src,
-  alt,
-  width = 300,
-  height = 180,
-}: ProjectImageProps) {
+export default function ProjectImage({ src, alt }: ProjectImageProps) {
   return (
-    <div className={`relative w-[${width}px] h-[${height}px] overflow-hidden`}>
+    <div className={`relative w-[300px] h-[180px] overflow-hidden`}>
       <Image
         fill
         src={src}
