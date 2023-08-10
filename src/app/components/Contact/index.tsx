e84fcf6e255aded3ba64d102c46eadeb810ecb32
Lorenzo <lorenzo.acetii@gmail.com>
2023-08-10 17:15:13 -0300

    Lg responsiviness on contact

--- src/app/components/Contact/index.tsx
+++ src/app/components/Contact/index.tsx
@@ -5,20 +5,17 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="flex flex-col items-center justify-center gap-4 my-40"
+      className="flex flex-col items-center justify-center gap-4 my-40 lg:my-20"
     >
       <h2 className="text-2xl text-amaranth-500 uppercase">
         Do you like my work?
       </h2>
       <ContactModal>
-        <div className="flex items-baseline gap-3 text-8xl text-amaranth-800 font-semibold cursor-pointer group">
+        <div className="flex items-baseline gap-3 text-8xl text-amaranth-800 font-semibold cursor-pointer group lg:text-6xl">
           <h1 className="relative after:duration-300 after:absolute after:bg-amaranth-800 after:h-[3px] after:w-0 after:left-1/2 after:-translate-x-1/2 after:-bottom-3 after:group-hover:w-full">
             {"Let's have a talk"}
           </h1>
-          <AiOutlineArrowRight
-            size={47}
-            className="group-hover:translate-x-2 duration-300"
-          />
+          <AiOutlineArrowRight className="group-hover:translate-x-2 duration-300 w-12 h-12 lg:w-10 lg:h-10" />
         </div>
       </ContactModal>
     </section>
