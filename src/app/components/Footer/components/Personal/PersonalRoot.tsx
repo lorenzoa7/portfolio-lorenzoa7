b2026c1731774c830e831a03c7844f5d4a231756
Lorenzo <lorenzo.acetii@gmail.com>
2023-08-14 14:09:22 -0300

    Sm responsiviness on footer

--- src/app/components/Footer/components/Personal/PersonalRoot.tsx
+++ src/app/components/Footer/components/Personal/PersonalRoot.tsx
@@ -4,7 +4,7 @@ type PersonalRootProps = {
 
 export default function PersonalRoot({ children }: PersonalRootProps) {
   return (
-    <section className="flex flex-col gap-2 w-full justify-start">
+    <section className="flex flex-col gap-2 w-full justify-start sm:items-start sm:gap-1">
       {children}
     </section>
   )
