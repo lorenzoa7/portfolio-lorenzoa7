4d3e56a1277f7b097fee760d96f166e079063f70
Lorenzo <lorenzo.acetii@gmail.com>
2023-08-14 12:05:59 -0300

    Xl responsiviness on skills

--- src/app/components/Skills/components/Level/LevelItems/LevelItemsRoot.tsx
+++ src/app/components/Skills/components/Level/LevelItems/LevelItemsRoot.tsx
@@ -4,7 +4,7 @@ type LevelItemsRootProps = {
 
 export default function LevelItemsRoot({ children }: LevelItemsRootProps) {
   return (
-    <div className="flex justify-center gap-7 mt-6 text-amaranth-500 group font-semibold text-lg lg:flex-wrap">
+    <div className="flex justify-center gap-7 mt-6 text-amaranth-500 group font-semibold text-lg xl:flex-wrap">
       {children}
     </div>
   )
