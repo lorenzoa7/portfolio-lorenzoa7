9eaec06fa5d8f932cd5ecabcbe3b54df5b98c56a
Lorenzo <lorenzo.acetii@gmail.com>
2023-08-14 18:20:48 -0300

    Add flex wrap to skills

--- src/app/components/Skills/components/Level/LevelItems/LevelItemsRoot.tsx
+++ src/app/components/Skills/components/Level/LevelItems/LevelItemsRoot.tsx
@@ -4,7 +4,7 @@ type LevelItemsRootProps = {
 
 export default function LevelItemsRoot({ children }: LevelItemsRootProps) {
   return (
-    <div className="flex justify-center gap-7 mt-6 text-amaranth-500 group font-semibold text-lg xl:flex-wrap sm:text-base sm:font-bold">
+    <div className="flex justify-center gap-7 mt-6 text-amaranth-500 group font-semibold text-lg flex-wrap sm:text-base sm:font-bold">
       {children}
     </div>
   )
