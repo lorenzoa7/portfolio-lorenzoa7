8160f2f03c0224d9ece472d82371b59bf103f35b
Lorenzo <lorenzo.acetii@gmail.com>
2023-08-17 19:35:03 -0300

    Adjust handle click on logo link

--- src/app/[lang]/components/Logo/LogoRoot.tsx
+++ src/app/[lang]/components/Logo/LogoRoot.tsx
@@ -1,12 +1,30 @@
+'use client'
+
 import Link from 'next/link'
 
 type LogoRootProps = {
   children: React.ReactNode
 }
 
+const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  e.preventDefault()
+
+  const href = e.currentTarget.href
+  const sectionId = href.replace(/.*#/, '')
+  const sectionElement = document.getElementById(sectionId)
+
+  sectionElement?.scrollIntoView({
+    behavior: 'smooth',
+  })
+}
+
 export function LogoRoot({ children }: LogoRootProps) {
   return (
-    <Link href="#about" className="relative flex gap-1 items-center w-fit">
+    <Link
+      href="#about"
+      className="relative flex gap-1 items-center w-fit"
+      onClick={(e) => handleClick(e)}
+    >
       {children}
     </Link>
   )
